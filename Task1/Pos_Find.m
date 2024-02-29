3206af991a1d074fecba2c1c9f9b6f3c565a9ab5
Joel Campo Moyà <joel.joel.campo@gmail.com>
2024-02-29 10:51:41 +0100

    init + fixnodes

---
+++ Task1/Pos_Find.m
@@ -0,0 +1,6 @@
+function [posicio] = Pos_Find(n_nod,DoF)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+posicio = (n_nod - 1)*DoF + 1;
+end
--- Task1/Task1.m
+++ Task1/Task1.m
@@ -6,3 +6,19 @@ clc;
 % Vehicles Aeroespacials. MUEA.
 % Task 1: Matrix structural analysis of a optical mount
 
+load("fe_model.mat");
+
+DoF = 6;
+nodes_fix = [10735; 13699; 16620; 19625; 22511; 4747];
+
+% PREALLOCATING
+fixnodes = zeros(size(nodes_fix,1)*DoF,3);
+
+for i = 1:size(nodes_fix,1)
+    for j = 1:DoF
+        fixnodes(j+(DoF*(i-1)),1) = nodes_fix(i);
+        fixnodes(j+(DoF*(i-1)),2) = j;
+    end
+    clear j
+end
+clear i
