91ef3d100b8e9709c20da995f85442caded99077
Joel Campo Moyà <joel.joel.campo@gmail.com>
2024-03-06 00:12:02 +0100

    Revert Commit 1_1

--- Task1/Task1_1.m
+++ Task1/Task1_1.m
@@ -74,13 +74,12 @@ F_D = K_DD * u_D + K_DN * u_N;
 
 F_D = reshape(F_D,[6,6]);
 
-u1 = zeros(size(n_tot,2),1);
-for i = 1:((size(n_tot,2)/DoF)-DoF)
-    u1(i) = u_N(6*(i-1)+1);
-end
+% PLOT META
 
-u1 = reshape(u1,[25390,6]);
-fillhdf('template.h5','static_X.h5',u1); % Plot META
+u_N_r = transpose(reshape(u_N,6,length(u_N)/6));
+
+fillhdf('template.h5','exercise_1_X.h5',u_N_r);
+
 
 % MASS COMPROVATION
 
