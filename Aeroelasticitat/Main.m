1c0376377fc2cede36a651984a5795cae0fd70e7
Joel Campo Moyà <joel.joel.campo@gmail.com>
2024-06-15 11:55:36 +0200

    100%

--- Aeroelasticitat/Main.m
+++ Aeroelasticitat/Main.m
@@ -35,6 +35,41 @@ k_5 = 2500; % N/m
 % Others
 rho = 1.25; %kg/m^3
 
+%% PART A - Aerodynamic modelling
+syms theta_1 theta_2 theta_3 delta U x y z t
+
+eqn_1 = -x/(pi * (c_1 - s)) - y/(-1/2 * pi * c_1) + U * theta_1 == 0;
+eqn_2 = -x/(3/2 * pi * c_1) - y/(pi * s) + U * (theta_1 + delta) == 0;
+
+sol = solve([eqn_1, eqn_2],[x, y]);
+
+gamma_1 = sol.x;
+gamma_delta = sol.y;
+
+eqn_3 = -z/(pi * c_1) - t/(pi * (3/2 * c_1 - 2 * x_1 - 2 * h_0 + 2 * x_2 - 1/2 * c_2)) + U * theta_2 == 0;
+eqn_4 = -z/(pi * (2 * x_1 + 2 * h_0 - 2 * x_2 + 3/2 * c_2 - 1/2 * c_1)) - t/(c_2 * pi) + U * theta_3 == 0;
+
+sol2 = solve([eqn_3, eqn_4],[z, t]);
+
+gamma_2 = sol2.z;
+gamma_3 = sol2.t;
+
+C = zeros(3,3);
+
+C(1,1) = rho * pi * c_1;
+
+coef1 = coeffs(gamma_2);
+C(2,2) = rho * double(coef1(2));
+C(2,3) = rho * double(coef1(1));
+
+coef2 = coeffs(gamma_3);
+C(3,3) = rho * double(coef2(1));
+C(3,2) = rho * double(coef2(2));
+
+coef_g1 = coeffs(gamma_1);
+coef_d = coeffs(gamma_delta);
+C_delta = 1.25 * (double(coef_g1(2)) + double(coef_d(2)));
+
 %% PART B - Structural dynamics modelling
 % Previous calculations
 m_1 = mu * h_1 * c_1;
@@ -132,18 +167,7 @@ S(4,2) = h_2;
 S(5,3) = h_2;
 
 % Aerodynamics Matrix computation
-
-C = zeros(3,3);
-
-C(1,1) = pi * c_1;
-
-C(2,2) = (-1)/((c_2/(4 * pi * ((3/4) * c_1 - (1/4) * c_2 - x_1 - h_0 + x_2) * ((3/4) * c_2 - (1/4) * c_1 + x_1 + h_0 - x_2))) - 1/(pi * c_1));
-C(2,3) = ((1 * c_2)/(2 * ((3/4) * c_1 - (1/4) * c_2 - x_1 - h_0 + x_2)))/((c_2/(4 * pi * ((3/4) * c_1 - (1/4) * c_2 - x_1 - h_0 + x_2) * ((3/4) * c_2 - (1/4) * c_1 + x_1 + h_0 - x_2))) - 1/(pi * c_1));
-
-C(3,2) = ((1 * c_1)/(2 * ((3/4) * c_2 - (1/4) * c_1 + x_1 + h_0 - x_2)))/((c_1/(4 * pi * ((3/4) * c_1 - (1/4) * c_2 - x_1 - h_0 + x_2) * ((3/4) * c_2 - (1/4) * c_1 + x_1 + h_0 - x_2))) - 1/(pi * c_2));
-C(3,3) = (-1)/((c_1/(4 * pi * ((3/4) * c_1 - (1/4) * c_2 - x_1 - h_0 + x_2) * ((3/4) * c_2 - (1/4) * c_1 + x_1 + h_0 - x_2))) - 1/(pi * c_2));
-
-C = rho * C;
+% C is calculated in section A
 
 C_coupling = zeros(3,5);
 
@@ -165,12 +189,11 @@ U_Div = real(U_Div_eigen(3));
 
 % Potential control reversal conditions (b)
 % Small calculations
-C_delta = 1.3149;
+% C_delta is calculated in section A
 delta_l_1 = 0.784375;
 delta_l_f = 0.530875;
 xp_delta_1 = (c_1 - 3/4 * s)/(4 - x_1);
-%xp_delta = x_1 - ((c_1 - s)/(4 * delta_l_1 + (c_1 - 3/4 * s) * delta_l_f));
-xp_delta = x_1 - ((c_1 - s)/4 * delta_l_1 + (c_1 - 3 * s/4) * delta_l_f);
+xp_delta = x_1 - ((c_1 - s)/4 * delta_l_1 + (c_1 - 3/4 * s) * delta_l_f);
 
 % Coupled System resolution
 S_delta = zeros(5,1);
@@ -195,7 +218,7 @@ for i = 1:length(U_discret)
 end
 
 % For better plotting (calculated after the first plotting)
-U_r = [12 12]; % Matching first U_r = 0 --> Aproximately
+U_r = [12.15 12.15]; % Matching first U_r = 0 --> At position 1216 of Delta_Lift_Ratio
 DeltaL_U_r = [-200 200]; % Matching final asymptote
 U_d = [U_Div U_Div];
 
@@ -276,12 +299,12 @@ for i = 1:length(U_discret)
 end
 
 % For better plotting (calculated after the first plotting)
-flutter = 6.23; % Matching first flutter = 0 --> Aproximately
+flutter_speed = 6.15; % Matching first flutter = 0 --> Position 616 of abs_real
 
 % Plotting real values
 figure 
 hold on
-xline(flutter,'--')
+xline(flutter_speed,'--')
 plot(U_discret,abs_real,'.')
 grid on
 legend('Flutter')
